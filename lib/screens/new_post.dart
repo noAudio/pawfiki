645de06a931b6e03242fd924812ed1094309821a
Samuel Odongo <samieodd@gmail.com>
2025-05-22 06:42:21 +0300

    Add text fields and buttons

--- lib/screens/new_post.dart
+++ lib/screens/new_post.dart
@@ -10,13 +10,45 @@ class NewPost extends StatelessWidget {
     return StoreConnector<AppState, dynamic>(
       converter: (store) => store.state,
       builder: (BuildContext context, state) {
-        return Center(
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Text(
-                'Welcome to the New Post page!',
-                style: Theme.of(context).textTheme.headlineMedium,
+              Column(
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: "Title",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Icon(Icons.photo),
+                        SizedBox(),
+                        Text("Add photo"),
+                      ],
+                    ),
+                  ),
+                  TextField(
+                    maxLines: 20,
+                    minLines: 5,
+                    decoration: InputDecoration(
+                      hintText: "Write your post details here",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {}, child: Text("Save Draft")),
+                  TextButton(onPressed: () {}, child: Text("Post")),
+                ],
               ),
             ],
           ),
