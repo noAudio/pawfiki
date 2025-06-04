6a1f6b1cfa2a0cff01e83ac4eb4952e789c89f40
Samuel Odongo <samieodd@gmail.com>
2025-06-04 12:29:39 +0300

    Add components to profile page

--- lib/screens/user_profile.dart
+++ lib/screens/user_profile.dart
@@ -10,16 +10,175 @@ class UserProfile extends StatelessWidget {
     return StoreConnector<AppState, dynamic>(
       converter: (store) => store.state,
       builder: (BuildContext context, state) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Welcome to the Profile page!',
-                style: Theme.of(context).textTheme.headlineMedium,
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Details",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Row(
+                        children: [
+                          Text("Email:"),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: TextFormField(
+                              readOnly: true,
+                              initialValue: "user@email.com",
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Username:"),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: TextFormField(
+                              readOnly: true,
+                              initialValue: "@username",
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text("Password"),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: TextFormField(
+                              readOnly: true,
+                              obscureText: true,
+                              initialValue: "password",
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                InkWell(
+                  splashColor: Theme.of(context).colorScheme.primary,
+                  hoverColor: Theme.of(context).colorScheme.primary,
+                  onTap: () {
+                    // TODO: Navigate to Edit Profile Screen
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.person),
+                            SizedBox(width: 10),
+                            Text("Edit profile details"),
+                          ],
+                        ),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  splashColor: Theme.of(context).colorScheme.primary,
+                  hoverColor: Theme.of(context).colorScheme.primary,
+                  onTap: () {
+                    // TODO: Navigate to Posts screen
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(18.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.newspaper),
+                            SizedBox(width: 10),
+                            Text("Posts"),
+                          ],
+                        ),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  splashColor: Theme.of(context).colorScheme.primary,
+                  hoverColor: Theme.of(context).colorScheme.primary,
+                  onTap: () {
+                    // TODO: Navigate to Replies screen
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(18.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.notifications),
+                            SizedBox(width: 10),
+                            Text("Comment replies"),
+                          ],
+                        ),
+                        Icon(Icons.arrow_forward),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            InkWell(
+              splashColor: Theme.of(context).colorScheme.error,
+              // hoverColor: Theme.of(context).colorScheme.error,
+              onTap: () {
+                // TODO: Navigate to Log In screen
+              },
+              child: Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.logout,
+                      color: Theme.of(context).colorScheme.error,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "Log out",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.error,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         );
       },
     );
