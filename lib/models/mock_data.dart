6b8fd0fcb62b5c6a0b27396316be5f3aeb143aa0
Samuel Odongo <samieodd@gmail.com>
2025-06-04 08:17:55 +0300

    Create mock data for ui testing

---
+++ lib/models/mock_data.dart
@@ -0,0 +1,62 @@
+import 'package:pawfiki/models/post_tag_enum.dart';
+
+List<Map<String, dynamic>> posts = [
+  {
+    "tag": PostTagEnum.update,
+    "commentCount": 2,
+    "userPic":
+        "https://images.pexels.com/photos/813940/pexels-photo-813940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "userName": "@andreaP",
+    "postTitle": "Fluffy is settling in perfectly 😊",
+    "postBodyPreview":
+        "A few weeks ago I posted about a stray cat who kept coming to my door. I took her in and...",
+    "postImageLink":
+        "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    "tag": PostTagEnum.spotted,
+    "commentCount": 6,
+    "userPic":
+        "https://images.pexels.com/photos/7163193/pexels-photo-7163193.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "userName": "@eliud",
+    "postTitle": "Spotted in Roysambu",
+    "postBodyPreview":
+        "Found a mama cat in Roysambu near TRM with two kittens. Not sure if they are strays. Anybody recognise...",
+    "postImageLink":
+        "https://images.pexels.com/photos/1416792/pexels-photo-1416792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    "tag": PostTagEnum.help,
+    "commentCount": 19,
+    "userPic":
+        "https://images.pexels.com/photos/1181694/pexels-photo-1181694.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "userName": "@christinaaaaa",
+    "postTitle": "How do I keep my cat busy",
+    "postBodyPreview":
+        "She won't let me work and will howl like a demon if I lock her out of my office!",
+    "postImageLink": "",
+  },
+  {
+    "tag": PostTagEnum.request,
+    "commentCount": 55,
+    "userPic":
+        "https://images.pexels.com/photos/2698935/pexels-photo-2698935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "userName": "@eben",
+    "postTitle": "Need to rehome my cat",
+    "postBodyPreview":
+        "Unfortunately I have cancer and 10 minutes left to live. Would somebody please take in my cat 😢...",
+    "postImageLink": "",
+  },
+  {
+    "tag": PostTagEnum.general,
+    "commentCount": 4,
+    "userPic":
+        "https://images.pexels.com/photos/8553648/pexels-photo-8553648.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "userName": "@iheartcats",
+    "postTitle": "My kitten is such  a picky eater!",
+    "postBodyPreview":
+        "He won't eat anything unless it is drenched in the blood of my enemies. Is anyone's cat also like...",
+    "postImageLink":
+        "https://images.pexels.com/photos/10404485/pexels-photo-10404485.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+];
