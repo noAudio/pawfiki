66f4140b08547c5b83d68ad5afa54675cfd11635
Samuel Odongo <samieodd@gmail.com>
2025-06-05 05:15:16 +0300

    Build widgets from resource list

--- lib/screens/resources.dart
+++ lib/screens/resources.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:pawfiki/components/resource_card.dart';
 import 'package:pawfiki/models/app_state.dart';
+import 'package:pawfiki/models/mock_data.dart';
 
 class Resources extends StatelessWidget {
   const Resources({super.key});
@@ -10,15 +12,25 @@ class Resources extends StatelessWidget {
     return StoreConnector<AppState, dynamic>(
       converter: (store) => store.state,
       builder: (BuildContext context, state) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Welcome to the Resources page!',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.builder(
+            itemCount: resourceList.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+            ),
+            itemBuilder: (_, index) {
+              return ResourceCard(
+                resourceName: resourceList[index]["name"],
+                resourceTypeEnum: resourceList[index]["type"],
+                resourceImageLink: resourceList[index]["resourceImage"],
+                phone: resourceList[index]["phone"],
+                websiteLink: resourceList[index]["website"],
+                mapLink: resourceList[index]["mapLink"],
+              );
+            },
           ),
         );
       },
