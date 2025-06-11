0562a3d2cbc55a3d33632a0c9d9f7a2882d1718e
Samuel Odongo <samieodd@gmail.com>
2025-06-11 11:24:15 +0300

    Expand user model to hold relevant data

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,7 @@
 class User {
   String username;
+  String email;
+  String password;
 
-  User({
-    required this.username
-  });
+  User({required this.username, required this.email, required this.password});
 }
