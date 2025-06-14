1c59536ee6857e1f7ee1388a1abe4edd11685d3d
Samuel Odongo <samieodd@gmail.com>
2025-06-14 16:30:10 +0300

    Create model for comments

---
+++ lib/models/comment.dart
@@ -0,0 +1,11 @@
+class Comment {
+  final String username;
+  final String userImageLink;
+  final String commentBody;
+
+  Comment({
+    required this.username,
+    required this.userImageLink,
+    required this.commentBody,
+  });
+}
